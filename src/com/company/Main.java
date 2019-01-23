bf7cc3de030ec4bd47902471a2cc5d7d5dadc0e7
Muhammad Shah <m_hussain_shah@hotmail.com>
2019-01-23 14:26:40 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,69 @@
+package com.company;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+/**
+ * You will continue creating an interactive chat-bot type program. Eliza is a therapist program that interacts with the
+ * user. Your program will need to evaluate what the user asks and turn the user's input into a question that sounds like
+ * the therapist really cares.
+ *
+ * Use HashMaps or String arrays to loop through user's input and implement the following:
+ *
+ * Replacements:
+ *
+ * replace i with you
+ * replace me with you
+ * replace my with your
+ * replace am with are
+ * We will continue to build on top of this application throughout the week.
+ *
+ * Here's how the replacement works:
+ *
+ * The user enters something at the prompt: "my teacher hates me"
+ *
+ * The program will loop through that string and replace "my" with "your" and "me" with "you" and then prepend the
+ * qualifier to the replacement string. So, my teacher hates me becomes "Why do you say that your teacher hates you?"
+ * The replacement method returns the same words as the user entered only the replacement words have been swapped. Then
+ * the qualifier is prepended to the user's words.
+ *
+ * Spend some time thinking how you would search through a string and replacing specific words. Hint: read about the
+ * split  (Links to an external site.)Links to an external site.operator.
+ *
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Map<String, String> myMap = new HashMap<>();
+        myMap.put("i","you");
+        myMap.put("me","you");
+        myMap.put("my","your");
+        myMap.put("am","are");
+
+	    Scanner keyboard = new Scanner(System.in);
+        String question, answer = "";
+        System.out.print("Good day. What is your problem? ");
+        while (true) {
+            System.out.print("Enter your response here or Q to quit: ");
+            question = keyboard.nextLine().toLowerCase();
+
+            for (String retval: question.split(" ")) {
+                if(myMap.containsKey(retval)){
+                    answer += myMap.get(retval) + " ";
+                } else {
+                    answer += retval + " ";
+                }
+            }
+
+            System.out.println(answer);
+
+            if (question.equalsIgnoreCase("I am feeling great") | question.equals("q")) {
+                break;
+            } else {
+                System.out.println(question);
+            }
+        }
+        System.out.println(">>> END");
+    }
+}
---
+++ src/com/company/SplitExample.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class SplitExample {
+    public static void main(String []args){
+        String Str = new String("I really like this Java course");
+        System.out.println("Return Value :" );
+
+        for (String retval: Str.split(" ")) {
+            System.out.println(retval);
+        }
+    }
+}
